31fc2ab304fe2458204aebf65fbc8c12b3c6df19
Alfredo Moralejo <amoralej@redhat.com>
2018-08-21 14:17:45 +0000

    Revert "Revert "Convert releng to use distroinfo instead of rdopkg""
    This reverts commit 3682af2e552b85fe751b6efae887c066f2f33bc8.
    
    Change-Id: Iebeea3066ca8f9d2afb1350a8f0243e372c2a07b

--- rdoutils/rdoinfo.py
+++ rdoutils/rdoinfo.py
@@ -2,7 +2,10 @@
 import os
 import re
 
-from rdopkg.actionmods import rdoinfo
+from distroinfo import info
+from distroinfo import query
+from rdopkg.utils import git
+from rdopkg import helpers
 
 local_info = os.environ['HOME'] + '/rdoinfo'
 if not os.path.exists(local_info):
@@ -10,10 +13,12 @@ if not os.path.exists(local_info):
 
 
 def get_projects(tag=None, buildsys_tag=None):
-    inforepo = rdoinfo.RdoinfoRepo(
-        local_repo_path=local_info)
-    inforepo.init(force_fetch=True)
-    all_packages = inforepo.get_info()['packages']
+    distroinfo = info.DistroInfo(
+        info_files='rdo.yml',
+        local_info=local_info)
+    inforepo = distroinfo.get_info()
+
+    all_packages = inforepo['packages']
     # If tag and buildys_tag are not specified it returns
     # all packages
     if tag is None and buildsys_tag is None:
@@ -49,10 +54,11 @@ def get_projects_distgit(tag=None, buildsys_tag=None):
 
 
 def get_pin(package, release):
-    inforepo = rdoinfo.RdoinfoRepo(
-        local_repo_path=local_info)
-    inforepo.init(force_fetch=True)
-    pkgs = [p for p in inforepo.get_info()['packages'] if p['name'] == package]
+    distroinfo = info.DistroInfo(
+        info_files='rdo.yml',
+        local_info=local_info)
+    inforepo = distroinfo.get_info()
+    pkgs = [p for p in inforepo['packages'] if p['name'] == package]
     if not pkgs or len(pkgs) != 1:
         raise NotInRdoinfo("Package %s not found in rdoinfo" % package)
     pkg = pkgs[0]
@@ -69,17 +75,20 @@ def get_pin(package, release):
 
 def get_new_pinned_builds(location, release):
     new_pins = []
-    inforepo = rdoinfo.RdoinfoRepo(
-            local_repo_path=location)
-    info1 = inforepo.get_info(gitrev='HEAD~')
-    info2 = inforepo.get_info()
-    packages = rdoinfo.tags_diff(info1, info2)
+    distroinfo = info.DistroInfo(
+        info_files='rdo.yml',
+        local_info=location)
+    info2 = distroinfo.get_info()
+    with helpers.cdir(location):
+        with git.git_revision('HEAD~'):
+            info1 = distroinfo.get_info()
+    packages = query.tags_diff(info1, info2, tagsname='tags')
     for package in packages:
         name = package[0]
         tags = package[1]
-        if release not in inforepo.get_package(name)['tags']:
+        if release not in query.get_package(info1, name)['tags']:
             break
-        pkg_tags = inforepo.get_package(name)['tags'][release]
+        pkg_tags = query.get_package(info2, name)['tags'][release]
         if release in tags and pkg_tags and 'source-branch' in pkg_tags.keys():
             pinned_version = pkg_tags['source-branch']
             new_pins.append({'name': name,
