ef35a01a0542ae5891fbfcea147c8ab40a61780e
Joel Capitao <jcapitao@redhat.com>
2022-10-20 18:29:17 +0200

    Update send_new_version.sh
    Change-Id: Iac38eb13acee6d29150cf3798f2699fa43784397

--- scripts/new_release_scripts/send_new_version.sh
+++ scripts/new_release_scripts/send_new_version.sh
@@ -1,38 +1,73 @@
+#!/bin/bash
+
+REALPATH=$(realpath "$0")
+DIRNAME=$(dirname "$REALPATH")
+source $DIRNAME/common.rc
+LANG=en_US.UTF-8
+
+function get_current_nvr_in_trunk() {
+    local pkg=$1
+    local component=$2
+    CURRENT_NVR_IN_TRUNK=$(repoquery --repofrompath=rdo,http://trunk.rdoproject.org/centos9-${MASTER_RELEASE}/component/$component/current/ --disablerepo=* --enablerepo=rdo --nvr $pkg 2>/dev/null)
+}
+
+
 PKG=$1
 
-version=train
+echo -e "Fetching $PKG medatadata from rdoinfo..."
+PACKAGE_INFO=$(rdopkg findpkg $PKG)
 
+echo -e "Cloning $PKG repo..."
 rm -rf $PKG
-
 rdopkg clone $PKG >/dev/null 2>&1
-pushd $PKG
-git remote update
-git checkout $version-rdo
-if [ $? != 0 ]
-then
-    git checkout --track origin/$version-rdo
-    if [ $? -ne 0 ]; then
-        echo "ERROR checking out $version-rdo in $PKG"
-        continue
-    fi 
-fi
-git pull
-TAG=`git describe --abbrev=0 upstream/stable/$version 2>/dev/null`
 if [ $? -ne 0 ]; then
-echo "NOT stable $version tag found, checking MASTER"
-TAG=`git describe --tag --abbrev=0 upstream/master 2>/dev/null`
+    echo "ERROR when cloning $PKG"
+    exit 1
 fi
-echo "New version detected $TAG"
-rdopkg findpkg $PKG |grep -A1 $version-uc
-#
 
+pushd $PKG >/dev/null
+git checkout -b $MASTER_RELEASE-rdo --track origin/$MASTER_RELEASE-rdo>/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: branch $MASTER_RELEASE-rdo does not exist in distgit repo"
+    exit 1
+else
+    echo "The branch ${MASTER_RELEASE}-rdo exists in distgit repo"
+fi
+
+LAST_NVR=$(echo "$PACKAGE_INFO" | grep cloud9s-openstack-${LATEST_RELEASE}-release|awk '{print $2}')
+LAST_VERSION=$(echo -e "$LAST_NVR" | rev | cut -d- -f2 | rev)
+echo -e "The latest NVR in ${LATEST_RELEASE} is:\t $LAST_NVR"
+
+CURRENT_TAG=$(echo "$PACKAGE_INFO" |grep -e "${MASTER_RELEASE}-uc"|awk '{print $2}')
+echo -e "The current tag is:\t\t $CURRENT_TAG"
+
+COMPONENT=$(echo "$PACKAGE_INFO" | grep -e "component"|awk '{print $2}')
+get_current_nvr_in_trunk $PKG $COMPONENT
+echo -e "Last build in RDO Trunk is:\t $CURRENT_NVR_IN_TRUNK"
+
+TAG=`git describe --abbrev=0 upstream/stable/$MASTER_RELEASE 2>/dev/null`
+if [ $? -ne 0 ]; then
+    echo "There is NOT stable tag found for '$MASTER_RELEASE', checking on 'master' tag."
+    TAG=`git describe --tag --abbrev=0 upstream/master 2>/dev/null`
+fi
+echo -e "Latest version detected:\t $TAG"
+
+if [ $TAG == $LAST_VERSION ]; then
+    echo "No new version detected, a cross-tag is required"
+    echo "cbs add-pkg cloud9s-openstack-$MASTER_RELEASE-candidate $PKG --owner=rdobuilder"
+    echo "cbs tag-build cloud9s-openstack-$MASTER_RELEASE-candidate $LAST_NVR"
+    echo "Press 2 key to run the 2 commands above:"
+    read -n 2
+    cbs add-pkg cloud9s-openstack-$MASTER_RELEASE-candidate $PKG --owner=rdobuilder
+    cbs tag-build cloud9s-openstack-$MASTER_RELEASE-candidate $LAST_NVR
+    exit 0
+fi
+
+rdopkg new-version -U -b $TAG -u RDO -e dev@lists.rdoproject.org -t -d
+
+echo "Press 2 key to submit review to Gerrit:"
 read -n 2
 
-rdopkg new-version -U -b $TAG -u RDO -e dev@lists.rdoproject.org -t
-if [ $? -eq 0 ]
-then
-    git review -t $version-branching
-else
-    echo "ERROR runing rdopkg new-version for $PKG"
-fi
-popd
+git review -t ${MASTER_RELEASE}-branching
+
+popd >/dev/null
