f7355333270648d8cd1405298e319aeac8a12efd
karolinku <kkula@redhat.com>
2023-10-05 11:48:59 +0200

    Script to update non-os puppet modules source-branch
    Script compares versions in Puppetfile with the ones in
    tags/release-uc.yaml and propose change if Puppetfile > pin.
    
    Change-Id: I9e4161e1c770390dd5795acb04c736b66cfd0dc8

---
+++ scripts/pin_non_os_puppet_modules.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+set -e
+
+# example execution
+#./scripts/pin_non_os_puppet_modules.sh "$GIT_BRANCH" "$UC_BRANCH"
+
+DIRNAME=$(basename $0 | cut -d. -f1)
+WORKDIR="/tmp/$DIRNAME"
+PUPPETFILE="$WORKDIR/Puppetfile"
+RDOINFO_FILE=$(pwd)
+
+GIT_TAG=${1:-stable/2024.1} # master, stable/zed, stable/2023.1...
+UC_BRANCH=${2:-caracal-uc} # caracal-uc, caracal, bobcat, antelope...
+
+if [ ! -d "$WORKDIR" ]; then
+    mkdir "$WORKDIR"
+else
+    rm -rf "$WORKDIR"
+    mkdir "$WORKDIR"
+fi
+
+cd "$WORKDIR"
+PUPPETFILE_URL="https://raw.githubusercontent.com/openstack/puppet-openstack-integration/$GIT_TAG/Puppetfile"
+
+curl -O -sS "$PUPPETFILE_URL"
+
+#transform puppetfile to parseable format
+sed -i -z 's/,\n//g' "$PUPPETFILE"
+sed -i "/^#.*/d" "$PUPPETFILE"
+sed -i '/^\s*$/d' "$PUPPETFILE"
+
+projects=$(cat "$PUPPETFILE" | awk '{print $2, $5, $8}' | tr -d "'" )
+
+while IFS= read -r line; do
+    if [[ ! $line =~ "master" ]] && [[ ! $line =~ "stable/" ]]; then
+        puppetfile_project=$(echo $line | awk '{print $1}')
+        puppetfile_version=$(echo $line | awk '{print $3}')
+        puppet_repo=$(echo $line | awk '{print $2}')
+        pinned_tag_uc=$(python3 -c "from rdoutils import rdoinfo; print(rdoinfo.get_pin(\"puppet-$puppetfile_project\", \"$UC_BRANCH\"))") || true
+
+         if [[ $pinned_tag_uc == "" ]]; then
+             echo "Project $puppetfile_project not exists, ignoring."
+             echo
+             continue
+        fi
+
+        if [[ $pinned_tag_uc == "None" ]]; then
+            echo "Project $puppetfile_project not pinned in rdoinfo, while puppetfile version is $puppetfile_version. Proposing pin."
+            echo
+            python3 -c "from rdoutils import rdoinfo; rdoinfo.update_tag('tags', \"puppet-$puppetfile_project\",\"$UC_BRANCH\", { 'source-branch':  \"$puppetfile_version\"}, local_dir=\"$RDOINFO_FILE\", update_all_files=False)"
+            continue
+        fi
+
+        if [ ! -d "$WORKDIR/$puppetfile_project" ]; then
+            git clone --quiet "$puppet_repo" "$WORKDIR/$puppetfile_project"
+        fi
+        pushd "$WORKDIR/$puppetfile_project" > /dev/null
+        if ! git merge-base --is-ancestor $puppetfile_version $pinned_tag_uc; then
+            echo "project: $puppetfile_project, version: $puppetfile_version, repo_url: $puppet_repo"
+            echo "Puppetfile version $puppetfile_version is newer than pinned tag $pinned_tag_uc. Proposing pin."
+            echo
+            python3 -c "from rdoutils import rdoinfo; rdoinfo.update_tag('tags', \"puppet-$puppetfile_project\",\"$UC_BRANCH\", {'source-branch':  \"$puppetfile_version\"}, local_dir=\"$RDOINFO_FILE\", update_all_files=False)"
+        else
+            echo "Puppetfile version $puppetfile_version of puppet-$puppetfile_project is older or equal to pinned $pinned_tag_uc. Not pinning."
+            echo
+        fi
+        popd > /dev/null
+    fi
+done <<< "$projects"
+
+rm -rf "$WORKDIR"
