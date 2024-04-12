3fad4e364f19be6b5bca7e9b0b3b51f84239bf8a
karolinku <kkula@redhat.com>
2024-04-12 15:40:22 +0200

    Add Caracal release to monitor in ftbfs dashboard
    Change-Id: I4b594b80bb174646329a540d9efcf80c5546fba0

--- rdoutils/generate_ftbfs_dashboard_feed.py
+++ rdoutils/generate_ftbfs_dashboard_feed.py
@@ -128,7 +128,7 @@ def main():
         releases = ["centos8-wallaby", "centos8-xena", "centos8-yoga",
                     "centos9-wallaby", "centos9-xena", "centos9-yoga",
                     "centos9-zed", "centos9-antelope", "centos9-bobcat",
-                    "centos9-master", "centos9-master-head"]
+                    "centos9-caracal", "centos9-master", "centos9-master-head"]
     else:
         releases = args.release
 
